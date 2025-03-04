09749d5a5a1b80a643d165b44740f876945ece4e
Alejandro <daleale.se@gmail.com>
2025-03-04 20:50:43 -0300

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -3,15 +3,17 @@ from .models import CarMake, CarModel
 
 
 # CarModelInline class
-class CarModelInline(admin.TabularInline):  # or use admin.StackedInline for a different layout
+class CarModelInline(admin.TabularInline):
     model = CarModel
     extra = 1  # Number of empty fields to add by default
+
 
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'car_make', 'type', 'year', 'dealer_id')
     list_filter = ('car_make', 'type', 'year')
     search_fields = ('name', 'car_make__name')
+
 
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):

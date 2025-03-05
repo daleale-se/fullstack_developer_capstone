79927c7fd09549fc830b07f0955394f50107e6c4
Alejandro <daleale.se@gmail.com>
2025-03-04 22:40:21 -0300

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -23,7 +23,7 @@ def get_request(endpoint, **kwargs):
     try:
         response = requests.get(request_url)
         return response.json()
-    except Exception as e::
+    except Exception as e:
         print(f"Error: {e}")
 
 

55879b0685937fea392510f3ebe12f36d42f4a4b
Alejandro <daleale.se@gmail.com>
2025-03-04 21:43:17 -0300

    Update 0001_initial.py

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -8,15 +8,14 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
             name='CarMake',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, 
-                primary_key=True, serialize=False, verbose_name='ID')),
+                    primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100, unique=True)),
                 ('description', models.TextField(blank=True, null=True)),
                 ('logo_url', models.URLField(blank=True, null=True)),
@@ -27,19 +26,19 @@ class Migration(migrations.Migration):
             name='CarModel',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, 
-                primary_key=True, serialize=False, verbose_name='ID')),
+                    primary_key=True, serialize=False, verbose_name='ID')),
                 ('dealer_id', models.IntegerField()),
                 ('name', models.CharField(max_length=100)),
                 ('type', models.CharField(choices=[('Sedan', 'Sedan'), 
-                ('SUV', 'SUV'), ('Wagon', 'Wagon'), ('Coupe', 'Coupe'), 
-                ('Convertible', 'Convertible'), ('Hatchback', 'Hatchback'), 
-                ('Pickup', 'Pickup')], default='Sedan', max_length=20)),
+                    ('SUV', 'SUV'), ('Wagon', 'Wagon'), ('Coupe', 'Coupe'), 
+                    ('Convertible', 'Convertible'), ('Hatchback', 'Hatchback'), 
+                    ('Pickup', 'Pickup')], default='Sedan', max_length=20)),
                 ('year', models.DateField()),
                 ('price', models.DecimalField(blank=True, decimal_places=2, 
-                max_digits=10, null=True)),
+                    max_digits=10, null=True)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('car_make', models.ForeignKey(on_delete=django.db.models
-                .deletion.CASCADE, related_name='models', to='djangoapp.carmake')),
+                ('car_make', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                    related_name='models', to='djangoapp.carmake')),
             ],
         ),
     ]

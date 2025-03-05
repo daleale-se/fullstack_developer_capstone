c587e0e6018788489a87b7e33636d6778a626b78
Alejandro <daleale.se@gmail.com>
2025-03-04 22:12:42 -0300

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -39,7 +39,7 @@ class CarModel(models.Model):
                             default=SEDAN)
     year = models.IntegerField()
 
-    price = models.DecimalField(max_digits=10, decimal_places=2, 
+    price = models.DecimalField(max_digits=10, decimal_places=2,
                                 blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 

6b96d7c925d53194d6ae8367b503dcd20b501a8f
Alejandro <daleale.se@gmail.com>
2025-03-04 21:56:38 -0300

    Update 0001_initial.py

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -14,8 +14,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarMake',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, 
-                    primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100, unique=True)),
                 ('description', models.TextField(blank=True, null=True)),
                 ('logo_url', models.URLField(blank=True, null=True)),

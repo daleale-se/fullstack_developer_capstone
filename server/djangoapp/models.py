8f9d110aec12e5ca333d98405e795af093c0aa67
Alejandro <daleale.se@gmail.com>
2025-03-04 22:11:59 -0300

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,21 +1,19 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
-from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class CarMake(models.Model):
-    name = models.CharField(max_length=100, unique=True)  
+    name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
-    logo_url = models.URLField(blank=True, null=True) 
+    logo_url = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name  # Return the name as the string representation
 
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name='models')
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE,
+                                 related_name='models')
     dealer_id = models.IntegerField()
     name = models.CharField(max_length=100)
 
@@ -26,7 +24,7 @@ class CarModel(models.Model):
     CONVERTIBLE = 'Convertible'
     HATCHBACK = 'Hatchback'
     PICKUP = 'Pickup'
-    
+
     CAR_TYPE_CHOICES = [
         (SEDAN, 'Sedan'),
         (SUV, 'SUV'),
@@ -36,11 +34,13 @@ class CarModel(models.Model):
         (HATCHBACK, 'Hatchback'),
         (PICKUP, 'Pickup'),
     ]
-    
-    type = models.CharField(max_length=20, choices=CAR_TYPE_CHOICES, default=SEDAN)
+
+    type = models.CharField(max_length=20, choices=CAR_TYPE_CHOICES,
+                            default=SEDAN)
     year = models.IntegerField()
 
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, 
+                                blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
